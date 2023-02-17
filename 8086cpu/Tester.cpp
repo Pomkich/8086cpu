50801e61149186aa2f49863a3316511ae9b42977
Роман Смирнов <pombl4.01@mail.ru>
2023-02-17 16:50:36 +0300

    implemented constructor of tester class, added getting memory pointer by function getMemPt

--- 8086cpu/Tester.cpp
+++ 8086cpu/Tester.cpp
@@ -1,1 +1,10 @@
 #include "Tester.h"
+
+Tester::Tester() {
+	cpu_pt = std::make_shared<cpu8086>();
+	mem_pt = cpu_pt->getMemPt();
+}
+
+void Tester::RunTests() {
+
+}
--- 8086cpu/Tester.h
+++ 8086cpu/Tester.h
@@ -1,7 +1,27 @@
 #pragma once
+#include "Constants.h"
+#include "cpu8086.h"
+#include "Memory.h"
+#include <memory>
 
-class Tester
-{
 
+class Tester {
+private:
+	std::shared_ptr<cpu8086> cpu_pt;
+	std::shared_ptr<cpu8086> mem_pt;
+
+public:
+	Tester();
+
+	void RunTests();
+
+	/*** MEMORY TESTS BEGIN ***/
+	/*** MEMORY TESTS END   ***/
+
+	/*** FLAG TESTS BEGIN ***/
+	/*** FLAG TESTS END   ***/
+
+	/*** OPCODE TESTS BEGIN ***/
+	/*** OPCODE TESTS END   ***/
 };
 
