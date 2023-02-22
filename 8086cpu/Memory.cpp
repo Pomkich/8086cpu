b1da37266ed1511e72695181cf8590a346e5c538
Роман Смирнов <pombl4.01@mail.ru>
2023-02-22 21:59:27 +0300

    Revert "fixed bug with stack push and pop"
    This reverts commit 5076f889beebca85b127db3f3b1e73909e36ac6d.

--- 8086cpu/Memory.cpp
+++ 8086cpu/Memory.cpp
@@ -12,10 +12,6 @@ word Memory::readW(dword address) {
 	return ((memory[address + 1] << 8) | memory[address]);
 }
 
-word Memory::readStack(dword address) {
-	return ((memory[address - 1] << 8) | memory[address]);
-}
-
 void Memory::writeB(dword address, byte val) {
 	memory[address] = val;
 }
@@ -24,9 +20,4 @@ void Memory::writeW(dword address, word val) {
 	// NOTE: следует добавить проверки на выход за пределы
 	memory[address] = (val & 0x00FF);		// нижняя часть слова
 	memory[address + 1] = (val >> 8);		// верхняя часть слова
-}
-
-void Memory::writeStack(dword address, word val) {
-	memory[address] = (val & 0x00FF);
-	memory[address - 1] = (val >> 8);
 }
--- 8086cpu/Memory.h
+++ 8086cpu/Memory.h
@@ -11,8 +11,6 @@ public:
 	Memory();
 	byte readB(dword address);
 	word readW(dword address);
-	word readStack(dword address);
 	void writeB(dword address, byte val);
 	void writeW(dword address, word val);
-	void writeStack(dword address, word val);
 };
