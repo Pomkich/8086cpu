11070b2ebbd2731a6142aa38ff8d9b85dde9f172
Роман Смирнов <pombl4.01@mail.ru>
2023-02-22 21:45:05 +0300

    fixed bug with stack push and pop

--- 8086cpu/Memory.cpp
+++ 8086cpu/Memory.cpp
@@ -12,6 +12,10 @@ word Memory::readW(dword address) {
 	return ((memory[address + 1] << 8) | memory[address]);
 }
 
+word Memory::readStack(dword address) {
+	return ((memory[address - 1] << 8) | memory[address]);
+}
+
 void Memory::writeB(dword address, byte val) {
 	memory[address] = val;
 }
@@ -20,4 +24,9 @@ void Memory::writeW(dword address, word val) {
 	// NOTE: следует добавить проверки на выход за пределы
 	memory[address] = (val & 0x00FF);		// нижняя часть слова
 	memory[address + 1] = (val >> 8);		// верхняя часть слова
+}
+
+void Memory::writeStack(dword address, word val) {
+	memory[address] = (val & 0x00FF);
+	memory[address - 1] = (val >> 8);
 }
