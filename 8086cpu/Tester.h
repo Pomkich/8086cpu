339d2fe37e286a735612944230d51a19d8d5e1d6
Роман Смирнов <pombl4.01@mail.ru>
2023-02-22 21:40:32 +0300

    found bug: push and pop from stack in not valid. Writing in memory by default methods writes values one byte higher

--- 8086cpu/Tester.h
+++ 8086cpu/Tester.h
@@ -32,6 +32,16 @@ public:
 	/*** FLAG TESTS END   ***/
 
 	/*** OPCODE TESTS BEGIN ***/
+	void ADD_R_IN_B_Test();
+	void ADD_R_OUT_B_Test();
+	void ADD_R_IN_W_Test();
+	void ADD_R_OUT_W_Test();
+	void ADD_A_B_Test();
+	void ADD_A_W_Test();
+	void INC_R_Test();
+	void DEC_R_Test();
+	void PUSH_R_Test();
+	void POP_R_Test();
 	/*** OPCODE TESTS END   ***/
 };
 
