69da20ee505fe3b379fbebb570193a744acdca6d
Роман Смирнов <pombl4.01@mail.ru>
2023-03-27 12:02:28 +0300

    fixed bug in O flag test

--- 8086cpu/main.cpp
+++ 8086cpu/main.cpp
@@ -11,7 +11,7 @@ int main() {
 	Tester test;
 	test.RunTests();
 
-	/*std::shared_ptr<cpu8086> cpu_pt = std::make_shared<cpu8086>();
+	std::shared_ptr<cpu8086> cpu_pt = std::make_shared<cpu8086>();
 	std::shared_ptr<Memory> mem_pt = std::make_shared<Memory>();
 	std::shared_ptr<AbstractPresenter> presenter_pt = std::make_shared<ConsolePresenter>(cpu_pt, mem_pt);
 	cpu_pt->initMemory(mem_pt);
@@ -27,20 +27,21 @@ int main() {
 	cpu_pt->setRegVal(RegId::SS, 0x3000);
 	// загрузка программы в память
 	mem_pt->writeB(0x10000, 0xB8);
-	mem_pt->writeB(0x10001, 0x32);
+	mem_pt->writeB(0x10001, 0x00);
 	mem_pt->writeB(0x10002, 0x00);
-	mem_pt->writeB(0x10003, 0xBB);
-	mem_pt->writeB(0x10004, 0x64);
+	mem_pt->writeB(0x10003, 0x2D);
+	mem_pt->writeB(0x10004, 0x01);
 	mem_pt->writeB(0x10005, 0x00);
-	mem_pt->writeB(0x10006, 0x03);
-	mem_pt->writeB(0x10007, 0xC3);
-	mem_pt->writeB(0x10008, 0xA3);
-	mem_pt->writeB(0x10009, 0x04);
-	mem_pt->writeB(0x10010, 0x00);
+	mem_pt->writeB(0x10006, 0xB8);
+	mem_pt->writeB(0x10007, 0xFF);
+	mem_pt->writeB(0x10008, 0xFF);
+	mem_pt->writeB(0x10009, 0x05);
+	mem_pt->writeB(0x1000A, 0x01);
+	mem_pt->writeB(0x1000B, 0x00);
 	// всего загружено 4 команды, 4 раза выполняем цикл
 	for (int i = 0; i < 4; i++) {
 		cpu_pt->clock();
-	}*/
+	}
 	
 
 	return 0;
