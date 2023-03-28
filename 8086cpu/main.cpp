f56b7d52c618462ce00ed9dd3ac946dce3812006
Роман Смирнов <pombl4.01@mail.ru>
2023-03-28 12:39:23 +0300

    added DAS command

--- 8086cpu/main.cpp
+++ 8086cpu/main.cpp
@@ -11,7 +11,7 @@ int main() {
 	Tester test;
 	test.RunTests();
 
-	std::shared_ptr<cpu8086> cpu_pt = std::make_shared<cpu8086>();
+	/*std::shared_ptr<cpu8086> cpu_pt = std::make_shared<cpu8086>();
 	std::shared_ptr<Memory> mem_pt = std::make_shared<Memory>();
 	std::shared_ptr<AbstractPresenter> presenter_pt = std::make_shared<ConsolePresenter>(cpu_pt, mem_pt);
 	cpu_pt->initMemory(mem_pt);
@@ -26,9 +26,9 @@ int main() {
 	cpu_pt->setRegVal(RegId::DS, 0x2000);
 	cpu_pt->setRegVal(RegId::SS, 0x3000);
 	// загрузка программы в память
-	mem_pt->writeB(0x10000, 0x04);
+	mem_pt->writeB(0x10000, 0x2C);		// opcode: sub
 	mem_pt->writeB(0x10001, 0xE0);
-	mem_pt->writeB(0x10002, 0x27);
+	mem_pt->writeB(0x10002, 0x2F);
 	// mem_pt->writeB(0x10003, 0x2D);
 	// mem_pt->writeB(0x10004, 0x01);
 	// mem_pt->writeB(0x10005, 0x00);
@@ -41,7 +41,7 @@ int main() {
 	// всего загружено 4 команды, 4 раза выполняем цикл
 	for (int i = 0; i < 2; i++) {
 		cpu_pt->clock();
-	}
+	}*/
 	
 
 	return 0;
