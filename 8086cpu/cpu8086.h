36d207a46bea1928388d0261c07b90474baefb49
Александр Смирнов <pombl4.01@mail.ru>
2023-04-15 17:51:40 +0300

    added jmp with condition commands

--- 8086cpu/cpu8086.h
+++ 8086cpu/cpu8086.h
@@ -138,6 +138,7 @@ private:
 	void DEC_R(word& reg);	// декремент регистра
 	void PUSH_R(word& reg);	// push 16 битного регистра в стек
 	void POP_R(word& reg);	// pop 16 битного регистра из стека
+	void JMP_COND(std::function<bool()> condition);	// переход по условию, условие - аргумент
 	void MOV_R_OUT_B();
 	void MOV_R_OUT_W();
 	void MOV_R_IN_B();
