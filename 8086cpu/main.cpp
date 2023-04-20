b10c79e17e547e8620fae1d5a76068cd11109958
Александр Смирнов <pombl4.01@mail.ru>
2023-04-20 17:34:11 +0300

    connected wxWidgets to project, start of GUI implementation

--- 8086cpu/main.cpp
+++ 8086cpu/main.cpp
@@ -1,5 +1,23 @@
-#include <iostream>
-#include "Tester.h"
+#include <wx/wx.h>
+
+class App : public wxApp {
+public:
+	bool OnInit() {
+		wxFrame* window = new wxFrame(NULL, wxID_ANY, "GUI Test", wxDefaultPosition, wxSize(600, 400));
+		wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+		wxStaticText* text = new wxStaticText(window, wxID_ANY, "Well Done!\nEverything seems to be working",
+			wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
+		text->SetFont(wxFont(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
+		sizer->Add(text, 1, wxALIGN_CENTER);
+		window->SetSizer(sizer);
+		window->Show();
+		return true;
+	}
+};
+
+wxIMPLEMENT_APP(App);
+
+/*#include "Tester.h"
 #include "cpu8086.h"
 #include "Memory.h"
 #include "AbstractPresenter.h"
@@ -45,3 +63,4 @@ int main() {
 
 	return 0;
 }
+*/
