5c4fde4e36e18bb8ced5d03144ba7e8957a1ae1c
Александр Смирнов <pombl4.01@mail.ru>
2023-05-03 18:07:04 +0300

    added prototype of GenerateLab function

---
+++ 8086cpu/TestingModule.cpp
@@ -0,0 +1,51 @@
+#include "TestingModule.h"
+
+void GenerateLab(
+	std::shared_ptr<cpu8086> cpu_pt,
+	std::shared_ptr<Memory> mem_pt,
+	std::shared_ptr<bool> running, 
+	std::list<RegId> regs, int mem_start, int mem_end, std::string deskription) {
+	// записать описание в файл
+	// создать тест:
+	//		сгенерировать входные данные для регистров и памяти
+	//		записать входные данные
+	//		запустить выполнение программы
+	//		прочитать и записать выходные данные
+	// создать ещё один текст
+
+
+	for (int test = 0; test < 10; test++) {
+		// генерируем случайные данные в регистры
+		std::cout << "input:" << std::endl;
+		dword value = 0;
+		for (auto id : regs) {
+			value = rand() % 65000;	// ~2^16
+			cpu_pt->setRegVal(id, value);
+			std::cout << value << std::endl;	// запись входных данных
+		}
+		// генерируем случайные данные в память
+		std::cout << "memory: " << std::endl;
+		for (int address = mem_start; address < mem_end; address++) {
+			mem_pt->writeB(address, rand() % 255);
+			std::cout << mem_pt->readB(address);	// запись в файл
+		}
+
+		// запускаем программу
+		while (*running) {
+			cpu_pt->clock();
+		}
+
+		std::cout << "out: " << std::endl;
+		// читаем выходные значения регистров
+		for (auto id : regs) {
+			value = cpu_pt->getRegVal(id);
+			std::cout << value << std::endl;
+		}
+		// читаем выходные значения памяти
+		std::cout << "memory: " << std::endl;
+		for (int address = mem_start; address < mem_end; address++) {
+			std::cout << mem_pt->readB(address);	// запись в файл
+		}
+	}
+
+}
---
+++ 8086cpu/TestingModule.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Constants.h"
+#include "AbstractPresenter.h"
+#include "cpu8086.h"
+#include "Memory.h"
+#include <list>
+#include <string>
+
+// функци€ генерации лабораторных, 
+// аргументы: 
+// указатель на процессор, 
+// указатель на пам€ть, 
+// указатель на переменную, котора€ говорит о прекращении работы процессора 
+// регистры, которые провер€ютс€, блок пам€ти и описание
+// предполагаетс€, что на момент вызова функции программа загружен в эмул€тор
+void GenerateLab(
+	std::shared_ptr<cpu8086> cpu_pt,
+	std::shared_ptr<Memory> mem_pt, 
+	std::shared_ptr<bool> running,
+	std::list<RegId> regs, int mem_start, int mem_end, std::string deskription);
+
+
+void VerifyLab(std::string lab_file_name) {};
