287f6f926e335916f1fdbe9222cdac56377a5326
Александр Смирнов <pombl4.01@mail.ru>
2023-05-04 12:24:35 +0300

    small code refactor at TestingModule

--- 8086cpu/TestingModule.cpp
+++ 8086cpu/TestingModule.cpp
@@ -13,15 +13,15 @@ void GenerateLab(
 	//		прочитать и записать выходные данные
 	// создать ещё один текст
 
-	boost::json::object out_file;
-	out_file["Description"] = description;
+	boost::json::object out_data;
+	out_data["Description"] = description;
 
 	word cs_copy = cpu_pt->getRegVal(RegId::CS);
 	word ip_copy = cpu_pt->getRegVal(RegId::IP);
 
 	for (int test = 0; test < 10; test++) {
 		std::string test_N = "test_" + std::to_string(test);
-		out_file[test_N].emplace_object();
+		out_data[test_N].emplace_object();
 
 		// восстанавливаем регистры сегментов (пока только кода)
 		cpu_pt->setRegVal(RegId::CS, cs_copy);
@@ -31,15 +31,15 @@ void GenerateLab(
 		for (auto id : regs) {
 			value = rand() % 65000;	// ~2^16
 			cpu_pt->setRegVal(id, value);
-			out_file[test_N].get_object()[std::to_string((int)id)] = { {"in", value}, {"out", 0} };
+			out_data[test_N].get_object()[std::to_string((int)id)] = { {"in", value}, {"out", 0} };
 		}
 		// генерируем случайные данные в память
-		out_file[test_N].get_object()["MemDumpIN"].emplace_array();	// инициализируем массив
+		out_data[test_N].get_object()["MemDumpIN"].emplace_array();	// инициализируем массив
 		//out_file["MemDumpIN"].emplace_array();	
 		for (int address = mem_start; address < mem_end; address++) {
 			value = rand() % 255;
 			mem_pt->writeB(address, value);
-			out_file[test_N].get_object()["MemDumpIN"].get_array().emplace_back(value);
+			out_data[test_N].get_object()["MemDumpIN"].get_array().emplace_back(value);
 		}
 
 		// запускаем программу
@@ -51,18 +51,19 @@ void GenerateLab(
 		// читаем выходные значения регистров
 		for (auto id : regs) {
 			value = cpu_pt->getRegVal(id);
-			out_file[test_N].get_object()[std::to_string((int)id)].get_object()["out"] = value;
+			out_data[test_N].get_object()[std::to_string((int)id)].get_object()["out"] = value;
 		}
 		// читаем выходные значения памяти
-		out_file[test_N].get_object()["MemDumpOUT"].emplace_array();
+		out_data[test_N].get_object()["MemDumpOUT"].emplace_array();
 		for (int address = mem_start; address < mem_end; address++) {
 			value = mem_pt->readB(address);
-			out_file[test_N].get_object()["MemDumpOUT"].get_array().emplace_back(value);
+			out_data[test_N].get_object()["MemDumpOUT"].get_array().emplace_back(value);
 		}
 	}
 	// записываем в файл структуру
 	std::ofstream file(lab_name + ".json");
-	file << boost::json::serialize(out_file);
+	file << boost::json::serialize(out_data);
+	file.close();
 }
 
 void VerifyLab(std::string lab_file_name) {
